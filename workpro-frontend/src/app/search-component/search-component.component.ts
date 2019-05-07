85787824b39096ef35523d25c3e6ba6fdca26790
biswadeep <biswadeep87@gmail.com>
2019-05-08 01:05:30 +0530

    workpro

---
+++ workpro-frontend/src/app/search-component/search-component.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductownerdetailsService } from '../productownerdetails.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-search-component',
+  templateUrl: './search-component.component.html',
+  styleUrls: ['./search-component.component.scss']
+})
+export class SearchComponentComponent implements OnInit {
+
+  id: String;
+  constructor(private projectService: ProductownerdetailsService, private route: ActivatedRoute) { }
+  projects;
+
+  ngOnInit() {
+    this.route.params.subscribe(data => {
+      this.id = data.id;
+    })
+    this.projectService.getResults(this.id).subscribe((data: any) => {
+      this.projects = data;
+    });
+  }
+
+}
